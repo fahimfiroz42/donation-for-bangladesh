d01dd41fb606f475318f4da115eb0c1d6a2f22ba
fahimfiroz42 <fahimfiroz.bd42@gmail.com>
2024-09-22 07:27:57 -0700

    boiler plate design done

---
+++ index.js
@@ -0,0 +1,28 @@
+const historyView=document.getElementById('history-form')
+const historyBtn=document.getElementById('history-btn')
+const donationBtn=document.getElementById('donation-btn')
+const donationForm=document.getElementById('donation-form')
+// history form functionality
+historyBtn.addEventListener('click',function(){
+
+
+    historyBtn.classList.add('bg-lightgreen')
+    historyView.classList.remove('hidden')
+    donationBtn.classList.remove('bg-lightgreen')
+    donationBtn.classList.add('text-lightdark','bg-white')
+    donationForm.classList.add('hidden')
+
+})
+
+// donation form back functionality
+
+
+donationBtn.addEventListener('click',function(){
+    
+    historyBtn.classList.remove('bg-lightgreen')
+    historyView.classList.add('hidden')
+    // historyBtn.classList.add('text-lightdark','bg-white')
+    donationBtn.classList.add('bg-lightgreen')
+    donationBtn.classList.remove('text-lightdark','bg-white')
+    donationForm.classList.remove('hidden')
+})
